406476b547be664cd93cd88956100093d029eb6d
tungchu1905 <chutrieutung2000@gmail.com>
2021-07-17 12:41:25 +0700

    exercise

---
+++ UserClass/UserClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            User u = new User();
+            Console.WriteLine("User's Infor: ");
+            Console.WriteLine(u.ToString());
+            Console.WriteLine();
+
+            ArrayList lIstOfUser = new ArrayList();
+            lIstOfUser.Add(new User("trungnhhe130147", "trungnhhe130147@fpu", 1));
+            lIstOfUser.Add(new User("trungnhhe130148", "trungnhhe130148@fpu", 1));
+            lIstOfUser.Add(new User("trungnhhe130149", "trungnhhe130149@fpu", 2));
+          
+
+            User u1 = new User("trungnhhe130148", "trungnhhe138148@fpu", 2);
+            Console.WriteLine("Index of u1 is ListOfUser: {0}\n", lIstOfUser.IndexOf(u1));
+            try
+            {
+                Object o = new Object();
+                Console.WriteLine("Index of o in ListOfUser: ");
+                Console.WriteLine(lIstOfUser.IndexOf(o));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserClass/UserClass/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UserClass
+{
+    class User
+    {
+        string username;
+        string email;
+        int roleid;
+
+        public User()
+        {
+            username = "tungcthe141487";
+            email = "tungcthe141487";
+            roleid = 1;
+        }
+
+        public User(string username, string email, int roleid)
+        {
+            this.username = username;
+            this.email = email;
+            this.roleid = roleid;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Email { get => email; set => email = value; }
+        public int Roleid { get => roleid; set => roleid = value; }
+
+        public override string ToString()
+        {
+            return "Username: " +username + " Email: "+email +" RoleID: " + roleid;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            User e = (User)obj;
+            return e.username == username;
+        }
+    }
+}
